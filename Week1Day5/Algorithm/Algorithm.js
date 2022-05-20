8dc33d4c02680bc91d0f4a79aacad4d018ab9d03
joshuabseaman <joshuabseaman@gmail.com>
2022-05-20 09:53:22 -0600

    Algorithm

---
+++ Week1Day5/Algorithm/Algorithm.js
@@ -0,0 +1,43 @@
+var fruit1 = "apples";
+var fruit2 = "oranges";
+    
+fruit2 = fruit1;
+    
+console.log(fruit2 + " and " + fruit1);
+//apples and apples
+
+
+var fruit1 = "apples";
+var fruit2 = "oranges";
+    
+var temp = fruit1; // temp is a common name for this
+fruit1 = fruit2;
+fruit2 = temp;
+    
+console.log(fruit2 + " and " + fruit1);
+//apples and oranges
+
+
+var start = 0;
+var end = 12;
+    
+while(start < end) {
+    console.log("start: " + start + ", end: " + end);
+    start += 2;
+    end -= 2;
+}
+//start: 0 end: 12
+//start: 2 end: 10
+//star: 4, end: 8
+
+
+function reverse(arr) {
+    for (var i = 0; i < arr.length/2; i++) {
+        var temp = arr[i];
+        arr[i] = arr[arr.length - 1 - i];
+        arr[arr.length - 1 - i] = temp;
+    }
+    return(arr);
+}
+
+console.log(reverse(["a", "b", "c", "d", "e"]));
